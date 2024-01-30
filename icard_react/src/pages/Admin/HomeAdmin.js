3c0656dcf9ec77442117dc478637a64a0f42e348
Andres Jimenez <andrezmonkey@gmail.com>
2024-01-30 11:22:47 -0500

    Cracion de topmenu y lateral menu

--- icard_react/src/pages/Admin/HomeAdmin.js
+++ icard_react/src/pages/Admin/HomeAdmin.js
@@ -4,8 +4,8 @@ import {useAuth} from "../../hooks";
 export function HomeAdmin() {
 const { logout } = useAuth();
   return (
-    <div>
-        <h1> Home Admin</h1> 
+    <div className='welcome-admin'>
+        <h1 className='welcome-admin-h1'>Hola</h1> 
         <button onClick={logout}>Cerrar Sesion</button>   
 
     </div>
